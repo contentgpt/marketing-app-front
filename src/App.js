421a57590fe1fdf0f9e7ffaf63c1f7c5ad56927d
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-03-13 11:42:08 -0700

    basic css positioning and color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Footer from './components/footer/Footer';
 import Content from './components/pages/Content';
 
 function App() {
   return (
     <div className="App">
       <Content/>
+      <Footer/>
     </div>
   );
 }
--- src/components/pages/Timeline.jsx
+++ src/components/pages/Timeline.jsx
@@ -4,11 +4,12 @@ import './Timeline.css';
 export default function Timeline({ messages }) {
   
   return (
-    <div>{messages.map((message) => (
-      <div className={`${message.role}`} key={message.content}>{message.content}
+    <>
+      <div className='messages-container'>{messages.map((message) => (
+        <div className={`${message.role}`} key={message.content}>{message.content}
+        </div>
+      ))}
       </div>
-    ))}
-    </div>
-
+    </>
   );
 }
